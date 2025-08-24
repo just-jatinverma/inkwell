8ae5828aa3b4c4c02867f20916e5bab752811bce
just-jatinverma <vermajatin1727@gmail.com>
2025-08-24 15:56:31 +0530

    admin controller completed

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,6 +1,9 @@
 import { ApiError } from '../utils/apiError';
 import { ApiResponse } from '../utils/apiResponse';
 import { asyncHandler } from '../utils/asyncHandler';
+import { Post } from '../models/posts.model';
+import { isValidObjectId } from 'mongoose';
+import { User } from '../models/user.model';
 
 const getAllPendingPosts = asyncHandler(async (req, res) => {
   // TODO: Implement logic to get all pending posts
@@ -9,7 +12,18 @@ const getAllPendingPosts = asyncHandler(async (req, res) => {
   // 3. Populate author details for each post.
   // 4. Implement pagination using query parameters (e.g., page, limit).
   // 5. Return the list of pending posts in an ApiResponse.
+  const page = parseInt(req.query.page as string, 10) || 1;
+  const limit = parseInt(req.query.limit as string, 10) || 1;
+  const skip = (page - 1) * limit;
+
+  const pendingPost = await Post.find({ status: 'pending' })
+    .populate('authorId', 'userName')
+    .skip(skip)
+    .limit(limit);
+
+  return res.status(200).json(new ApiResponse(200, pendingPost, 'success'));
 });
+
 const approvePost = asyncHandler(async (req, res) => {
   // TODO: Implement logic to approve a post
   // 1. This should be an admin-only route.
@@ -19,6 +33,30 @@ const approvePost = asyncHandler(async (req, res) => {
   // 5. Update the post's status to 'published'.
   // 6. Save the updated post.
   // 7. Return a success message in an ApiResponse.
+
+  const { postId } = req.params;
+
+  if (!isValidObjectId(postId)) {
+    throw new ApiError(400, 'invalid Id');
+  }
+
+  const post = await Post.findById(postId);
+
+  if (!post) {
+    throw new ApiError(400, 'post not found');
+  }
+
+  if (post.status === 'approved') {
+    throw new ApiError(400, 'already approved post');
+  }
+
+  const updatedPost = await Post.findByIdAndUpdate(
+    postId,
+    { $set: { status: 'approved' } },
+    { new: true }
+  );
+
+  return res.status(200).json(new ApiResponse(200, updatedPost, 'success'));
 });
 const rejectPost = asyncHandler(async (req, res) => {
   // TODO: Implement logic to reject a post
@@ -31,6 +69,29 @@ const rejectPost = asyncHandler(async (req, res) => {
   // 7. Save the updated post.
   // 8. (Optional) Notify the author about the rejection and the reason.
   // 9. Return a success message in an ApiResponse.
+  const { postId } = req.params;
+
+  if (!isValidObjectId(postId)) {
+    throw new ApiError(400, 'invalid Id');
+  }
+
+  const post = await Post.findById(postId);
+
+  if (!post) {
+    throw new ApiError(400, 'post not found');
+  }
+
+  if (post.status === 'rejected') {
+    throw new ApiError(400, 'already rejected post');
+  }
+
+  const updatedPost = await Post.findByIdAndUpdate(
+    postId,
+    { $set: { status: 'rejected' } },
+    { new: true }
+  );
+
+  return res.status(200).json(new ApiResponse(200, updatedPost, 'success'));
 });
 
 const promoteToAdmin = asyncHandler(async (req, res) => {
@@ -42,6 +103,17 @@ const promoteToAdmin = asyncHandler(async (req, res) => {
   // 5. Update the user's role to 'admin'.
   // 6. Save the updated user.
   // 7. Return a success message and the updated user's info (without sensitive data) in an ApiResponse.
+  const { userId } = req.body;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(400, 'user not found');
+  }
+
+  const promoted = await User.findByIdAndUpdate(userId, { $set: { role: 'admin' } }, { new: true });
+
+  return res.status(200).json(new ApiResponse(200, promoted, 'success'));
 });
 
 export { getAllPendingPosts, approvePost, rejectPost, promoteToAdmin };
